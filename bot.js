d55d9e3eb1041bbe4e0aff7861d2bda882abca5e
RickenKlawtek <52252784+RickenKlawtek@users.noreply.github.com>
2021-01-21 17:11:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const CommandoClient = require('./client');
 const path = require('path');
 const fs = require('fs')
-//const dotenv = require('dotenv')
+const dotenv = require('dotenv')
 
 //const envConfig = dotenv.parse(fs.readFileSync('.env'))
 //for (const k in envConfig) {
